7200260d185adc3d7e5065aa2be8c0e4ad8bf0cd
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2021-02-26 21:50:31 -0600

    Added do-while loop to pull in all songs from multipage API

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 require('dotenv').config();
 const axios = require('axios');
 const { getLyrics, getSong } = require('genius-lyrics-api');
+const get = require('lodash/get');
 
 const { GENIUS_ACCESS_TOKEN } = process.env;
 
-// const options = {
-// 	apiKey: GENIUS_ACCESS_TOKEN,
-// 	title: 'Blinding Lights',
-// 	artist: 'Waxahatchee',
-// 	optimizeQuery: true
-// };
-
 const retrieveWaxahatcheeData = async () => {
-    const response = await axios.get('https://api.genius.com/artists/68428/songs', {
-        params: {
-            per_page: 50,
-            page: 2
-        },
-        headers: {
-            Authorization: `Bearer ${GENIUS_ACCESS_TOKEN}`
-        }
-    })
+    let page = 1;
+    let songs = [];
+    let latestResult;
+
+    do {
+        const response = await axios.get('https://api.genius.com/artists/68428/songs', {
+            params: {
+                per_page: 50,
+                page
+            },
+            headers: {
+                Authorization: `Bearer ${GENIUS_ACCESS_TOKEN}`
+            }
+        })
+
+        latestResult = get(response, 'data.response');
+
+        const batchOfSongs = get(response, 'data.response.songs');
+
+        batchOfSongs.forEach(song => songs.push(song));
+
+        page++
+    } while (latestResult.next_page !== null)
 
     const bracketsRegEx = /\[.*?\]/g
 
-    console.log(response.data)
+    console.log(songs.length)
 
     // const lyrics = await getLyrics(songs[10].url);
 
